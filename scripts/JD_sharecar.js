e0bdd98099b6b50e77ea2b430000a3f3f742e53e
U_GOLD <63148494+ZeroMah@users.noreply.github.com>
2020-12-22 13:13:13 +0800

    Update JD_sharecar.js

--- scripts/JD_sharecar.js
+++ scripts/JD_sharecar.js
@@ -13,7 +13,7 @@ const shareCodes = [
     mc: $.getdata("mc_shareCode1") || "MTAxODc2NTEzNDAwMDAwMDAyNzkyNjM4Mw==",
     ddgc: $.getdata("dd_shareCode1") || "P04z54XCjVWnYaS5kZ7fCKtjCEX",
     jxgc: $.getdata("jx_shareCode1") || "k3XRgh9SqTEODDhQVrfL1A==",
-    jdzz: $.getdata("zz_shareCode1") || "",
+    jdzz: $.getdata("zz_shareCode1") || "S5KkcRBlP_VLeIk6nl_MDJg",
   },
   {
     zd: $.getdata("zd_shareCode2") || "",
@@ -21,7 +21,7 @@ const shareCodes = [
     mc: $.getdata("mc_shareCode2") || "",
     ddgc: $.getdata("dd_shareCode2") || "",
     jxgc: $.getdata("jx_shareCode2") || "",
-    jdzz: $.getdata("zz_shareCode1") || "",
+    jdzz: $.getdata("zz_shareCode2") || "",
   },
 ];
 $.result = [];
@@ -30,7 +30,7 @@ $.random = Math.floor(Math.random() * 60);
 !(async () => {
   console.log(`\n此脚本延迟${$.random}秒执行\n`);
   for (let i = 0; i < shareCodes.length; i++) {
-    const { zd, nc, mc, ddgc, jxgc ,jdzz, } = shareCodes[i];
+    const { zd, nc, mc, ddgc, jxgc,jdzz, } = shareCodes[i];
     await $.wait($.random);
     zd &&
       (await create(
@@ -60,9 +60,11 @@ $.random = Math.floor(Math.random() * 60);
       (await create(
         `http://api.turinglabs.net/api/v1/jd/jxfactory/create/${jxgc}/`,
         "京喜工厂"
+      ));  
+    await $.wait($.random);  
     jdzz &&
       (await create(
-        https://code.chiang.fun/api/v1/jd/jdzz/create/${jdzz}/,
+        `https://code.chiang.fun/api/v1/jd/jdzz/create/${jdzz}/`,
         "京东赚赚" 
       ));
   }
